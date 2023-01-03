b44e0ca5c8124a580b68272d4a3399f2af7afc46
Fakhar Abbas <afakhar363@gmail.com>
2023-01-03 10:42:02 +0500

    Some Important Changes

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const searchMessage = () => {
 //search chat
 messageSearch.addEventListener("keyup", searchMessage);
 
-search Feed
+// search Feed
 const searchFeed = () => {
   const val = feedSearch.value.toLowerCase();
   feed.forEach((chat) => {
@@ -509,7 +509,7 @@ postForm.addEventListener("submit", (e) => {
 //Delete Post
 
 // threeDots.addEventListener(cli);
-let j = 0;
+let j = 10;
 
 //Get new post from the FAKE API
 async function getNewPost(e) {
@@ -522,16 +522,10 @@ async function getNewPost(e) {
   const allPosts = await res.json();
   // .then((res) => res.json())
 
-  for (let i = 0; i < 10; i++) {
+  for (var i = 0; i < 10; i++) {
     // console.log(allPosts.posts[i], allPosts.posts[i].id + 1);
-    const feeds = document.querySelector(".feeds");
-    const div = document.createElement("div");
-    div.classList.add("feed");
-    j += 1;
-
     //Comments
-
-    const HTML1 = `
+    var HTML1 = `
     <div class="head">
       <div class="user">
         <div class="profile-picture">
@@ -540,7 +534,7 @@ async function getNewPost(e) {
           }.jpg" alt="Profile Picture" />
         </div>
         <div class="ingo">
-          <h3>alpha</h3>
+          <h3>${allPosts.posts[i].userId}</h3>
           <small>${allPosts.posts[i].title}</small>
         </div>
       </div>
@@ -590,6 +584,9 @@ async function getNewPost(e) {
     </div>
     `;
 
+    const feeds = document.querySelector(".feeds");
+    const div = document.createElement("div");
+    div.classList.add("feed");
     div.innerHTML = HTML1;
     feeds.appendChild(div);
     let response = await fetch(`https://dummyjson.com/comments/post/${i + 1}`, {
@@ -629,10 +626,70 @@ async function getNewPost(e) {
 
       // div2.insertAdjacentHTML("beforeend", HTML2);
     }
+
+    j += 1;
   }
+
+  window.onscroll = async function (ev) {
+    if (
+      window.innerHeight + window.pageYOffset >=
+      document.body.offsetHeight - 2
+    ) {
+      for (i; i < 20; i++) {
+        const feeds = document.querySelector(".feeds");
+        const div = document.createElement("div");
+        div.classList.add("feed");
+        div.innerHTML = HTML1;
+        feeds.appendChild(div);
+        let response = await fetch(
+          `https://dummyjson.com/comments/post/${i + 1}`,
+          {
+            method: "GET",
+          }
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        for (let m = 0; m < data.comments.length; m++) {
+          console.log(data.comments[m].body);
+
+          const div2 = document.createElement("div");
+
+          const HTML2 = `
+        <div class="comment-block"><img class="comment-image" src="./images/profile-${
+          i + 1
+        }.jpg"/>
+    <div class="comment-dialog">
+      <h4 class="username">Matheus Plessmann</h4>
+      <p class="text">${data.comments[m].body}</p>
+    </div>
+    <button class="btn btn-primary cmt-btn" onClick="onDelete(this)">Delete</button>
+  </div>
+          
+        <!-- ---------------------------1st Feed end------------------- -->
+`;
+
+          const feeds = document.querySelector(".feeds");
+          const div = document.createElement("div");
+          div.classList.add("feed");
+
+          const feed = document.querySelectorAll(".feed");
+          feed.forEach((feed) => {
+            div2.innerHTML = HTML2;
+            feed.appendChild(div2);
+          });
+
+          // div2.insertAdjacentHTML("beforeend", HTML2);
+        }
+      }
+      console.log(i);
+    }
+  };
+
   //Feed Search
   const feedSearch = document.querySelector("#feed-search");
-  const feeds = document.querySelector(".feeds");
+  // const feeds = document.querySelector(".feeds");
   const feed = document.querySelectorAll(".feed");
   //   console.log(feed);
   const searchFeed = () => {
@@ -687,302 +744,6 @@ function refreshPosts() {
   else if (window.attachEvent) window.attachEvent("onload", getNewPost);
 }
 window.onload = refreshPosts();
-
-//Get post when scrolled
-if (window.innerHeight + 50 < document.body.scrollHeight) {
-  setTimeout(
-    (window.onscroll = function (ev) {
-      if (
-        window.innerHeight + window.pageYOffset >=
-        document.body.offsetHeight - 2
-      ) {
-        // alert("you're at the bottom of the page");
-        async function getNewPostOnScroll(e) {
-          //   e.preventDefault();
-          const res = await fetch("https://dummyjson.com/posts", {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-            // body:JSON.stringify()
-          });
-          const allPosts = await res.json();
-          // .then((res) => res.json())
-          for (j; j < 20; j++) {
-            // console.log(allPosts.posts[j], allPosts.posts[j].id + 1);
-            const feeds = document.querySelector(".feeds");
-            const div = document.createElement("div");
-            div.classList.add("feed");
-
-            const HTML = `
-                      <div class="head">
-                        <div class="user">
-                          <div class="profile-picture">
-                            <img src="./images/profile-${
-                              allPosts.posts[j].id + 1
-                            }.jpg" alt="Profile Picture" />
-                          </div>
-                          <div class="ingo">
-                            <h3>Beta${[j]}</h3>
-                            <small>${allPosts.posts[j].title}</small>
-                          </div>
-                        </div>
-                        <span class="edit">
-                          <i class="uil uil-ellipsis-h"></i>
-                        </span>
-                      </div>
-                      <div class="photo">
-                      ${allPosts.posts[j].body}
-                        
-                      </div>
-                      <div class="action-buttons">
-                        <div class="intraction-buttons">
-                        <span class="like-btn" >
-                        <i class="fa-regular fa-heart active"></i
-                        ><i class="fa-solid fa-heart"></i>
-                      </span>
-                          <span>
-                            <i class="uil uil-comment-dots"></i>
-                          </span>
-                          <span>
-                            <i class="uil uil-share-alt"></i>
-                          </span>
-                        </div>
-                        <div class="bookmark">
-                          <span>
-                            <i class="uil uil-bookmark-full"></i>
-                          </span>
-                        </div>
-                      </div>
-                      <div class="liked-by"><p>Liked by <b>${
-                        allPosts.posts[j].reactions
-                      } others</b></p>
-                      </div>
-                      <div class="caption">
-                        <p>
-                          <b> Lorem ipsum</b> dolor sit amet consectetur adipisicing
-                          elit.
-                          <span class="hash-tag">#trending</span>
-                        </p>
-                      </div>
-                      <!-- ---------------------------1st Feed end------------------- -->
-          `;
-            //   const feed = (document.querySelector(".feed").insertAdjacentHTML =
-            // ("afterbegin", HTML));
-            div.innerHTML = HTML;
-            feeds.appendChild(div);
-            let response = await fetch(
-              `https://dummyjson.com/comments/post/${j}`,
-              {
-                method: "GET",
-              }
-            );
-            if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const data = await response.json();
-            console.log(data.comments.length);
-            for (let m = 0; m < data.comments.length; m++) {
-              console.log(data.comments[m].body);
-
-              const div2 = document.createElement("div");
-
-              const HTML2 = `
-        <div class="comment-block"><img class="comment-image" src="./images/profile-${
-          j + 1
-        }.jpg"/>
-    <div class="comment-dialog">
-      <h4 class="username">Matheus Plessmann</h4>
-      <p class="text">${data.comments[m].body}</p>
-    </div>
-    <button class="btn btn-primary cmt-btn" onClick="onDelete(this)">Delete</button>
-  </div>
-          
-        <!-- ---------------------------1st Feed end------------------- -->
-  `;
-              //   const feed = (document.querySelector(".feed").insertAdjacentHTML =
-              // ("afterbegin", HTML));
-
-              const feed = document.querySelectorAll(".feed");
-              feed.forEach((feed) => {
-                div2.innerHTML = HTML2;
-                feed.appendChild(div2);
-              });
-
-              // div2.insertAdjacentHTML("beforeend", HTML2);
-            }
-          }
-          //Feed Search
-          const feedSearch = document.querySelector("#feed-search");
-          const feeds = document.querySelector(".feeds");
-          const feed = document.querySelectorAll(".feed");
-          //   console.log(feed);
-          const searchFeed = () => {
-            const val = feedSearch.value.toLowerCase();
-            feed.forEach((chat) => {
-              let name = chat
-                .querySelector("h3")
-                .textContent.toLocaleLowerCase();
-              let feedText = chat
-                .querySelector(".photo")
-                .textContent.toLocaleLowerCase();
-              if (name.indexOf(val) != -1 || feedText.indexOf(val) != -1) {
-                chat.style.display = "content";
-              } else {
-                chat.style.display = "none";
-              }
-            });
-          };
-
-          //search chat
-          feedSearch.addEventListener("keyup", searchFeed);
-          const commentInput = document.querySelectorAll(".comment-text");
-          //   console.log(commentInput);
-          const addCommentBtn = document.querySelectorAll(".add-comment-btn");
-          //   console.log(addCommentBtn);
-
-          const commentCountText = document.querySelectorAll(
-            ".comment-count-text"
-          );
-          //   console.log(commentCountText);
-
-          //Add Comment
-
-          for (let i = 0; i < addCommentBtn.length; i++) {
-            addCommentBtn[i].addEventListener("click", () => {
-              // for (let j = 0; j < commentInput.length; j++) {
-              let newComment = createComment(commentInput[i].value);
-              commentCountText[i].insertAdjacentElement(
-                "beforebegin",
-                newComment
-              );
-              // }
-              // for (let i = 0; i < commentCountText.length; i++) {
-              commentCountText[i].insertAdjacentElement(
-                "beforebegin",
-                newComment
-              );
-              // }
-            });
-          }
-          //   likeSetting();
-
-          //   console.log(allPosts);
-          //   console.log(allPosts.posts[0]);
-        }
-        getNewPostOnScroll();
-        //Toggle comment btn
-        const commentBtn = document.querySelectorAll(".Comment-btn");
-        //   console.log(commentBtn);
-        const commentSection = document.querySelectorAll(".comment-section");
-        //   console.log(commentSection);
-        console.log("sdfg", commentBtn.length);
-
-        for (let i = 1; i < commentBtn.length; i++) {
-          commentBtn[i].addEventListener("click", function () {
-            //   for (let p = 0; p < commentSection.length; p++) {
-            commentSection[i].classList.toggle("comment-section");
-            //   }
-          });
-        }
-      }
-    }),
-    2000
-  );
-}
-
-//LIke
-// function likeSetting() {
-//   const likeBtn = document.querySelectorAll(".like-btn");
-//   console.log(likeBtn);
-//   for (let i = 0; i < likeBtn.length; i++) {
-//     likeBtn[i].addEventListener("click", function () {
-//       const emptyHeart = document.querySelectorAll(".fa-regular");
-//       const fillHeart = document.querySelectorAll(".fa-solid");
-//       if (emptyHeart[i].classList.contains("active")) {
-//         emptyHeart[i].classList.remove("active");
-//         emptyHeart[i].style.display = "none";
-//         fillHeart[i].style.display = "inline-block";
-//         fillHeart[i].classList.add("active");
-//       } else if (fillHeart[i].classList.contains("active")) {
-//         emptyHeart[i].classList.add("active");
-//         emptyHeart[i].style.display = "inline-block";
-//         fillHeart[i].style.display = "none";
-//         fillHeart[i].classList.remove("active");
-//       }
-//     });
-//   }
-// }
-// likeSetting();
-// const emptyHeart = document.querySelectorAll(".fa-regular");
-// console.log(emptyHeart);
-// const fillHeart = document.querySelectorAll(".fa-solid");
-// console.log(fillHeart);
-// function clickme() {
-//   for (let i = 0; i < emptyHeart.length; i++) {
-//     if (emptyHeart[i].classList.contains("active")) {
-//       emptyHeart[i].classList.remove("active");
-//       emptyHeart[i].style.display = "none";
-//       fillHeart[i].style.display = "inline-block";
-//       fillHeart[i].classList.add("active");
-//     } else if (fillHeart[i].classList.contains("active")) {
-//       emptyHeart[i].classList.add("active");
-//       emptyHeart[i].style.display = "inline-block";
-//       fillHeart[i].style.display = "none";
-//       fillHeart[i].classList.remove("active");
-//     }
-//   }
-// }
-
-//Toggle comment btn
-// const commentBtn = document.querySelectorAll(".Comment-btn");
-// console.log(commentBtn);
-// const commentSection = document.querySelectorAll(".comment-section");
-// console.log(commentSection);
-
-// for (let i = 0; i < commentBtn.length; i++) {
-//   commentBtn[i].addEventListener("click", function () {
-//     for (let j = 0; j < commentSection.length; j++) {
-//       commentSection[j].classList.toggle("comment-section");
-//     }
-//   });
-// }
-
-// //Add Comment
-
-// const commentInput = document.querySelectorAll(".comment-text");
-// console.log(commentInput);
-// const addCommentBtn = document.querySelectorAll(".add-comment-btn");
-// console.log(addCommentBtn);
-
-// const commentCountText = document.querySelectorAll(".comment-count-text");
-// console.log(commentCountText);
-
-// const createComment = (title) => {
-//   const newComment = document.createElement("div");
-//   newComment.classList.add("Comment-area");
-
-//   newComment.appendChild(document.createElement("p")).textContent = title;
-//   const btnParentDiv = newComment.appendChild(document.createElement("div"));
-//   const button = btnParentDiv.appendChild(document.createElement("button"));
-//   const editbutton = btnParentDiv.appendChild(document.createElement("button"));
-//   editbutton.classList.add("text-muted");
-//   editbutton.textContent = "Edit";
-//   button.classList.add("text-muted");
-//   button.textContent = "Delete";
-//   button.addEventListener("click", () => newComment.remove());
-//   return newComment;
-//   //   newComment.
-// };
-// for (let i = 0; i < addCommentBtn.length; i++) {
-//   addCommentBtn[i].addEventListener("click", () => {
-//     // for (let j = 0; j < commentInput.length; j++) {
-//     let newComment = createComment(commentInput[i].value);
-//     commentCountText[i].insertAdjacentElement("beforebegin", newComment);
-//     // }
-//     // for (let i = 0; i < commentCountText.length; i++) {
-//     commentCountText[i].insertAdjacentElement("beforebegin", newComment);
-//     // }
-//   });
-// }
 
 setInterval(likeSetting, 2000);
 // setTimeout(alpha, 2000);
